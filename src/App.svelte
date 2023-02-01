fd04b51e6374c1aa3d9bed04bbb799c52c7758a1
Sofa <Nextil3o3@gmail.com>
2023-02-01 08:30:51 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { gameOver } from "./store";
+  import { fade } from "svelte/transition";
+  import Board from "./components/Board.svelte";
+  import Keyboard from "./components/Keyboard.svelte";
+  import GameOver from "./components/GameOver.svelte";
+</script>
+
+<main>
+  <nav>Space Wordle</nav>
+
+  {#if $gameOver}
+    <div in:fade={{ duration: 1000 }}>
+      <GameOver />
+    </div>
+  {:else}
+    <div in:fade={{ duration: 800 }}>
+      <Board />
+      <Keyboard />
+    </div>
+  {/if}
+</main>
+
+<style>
+  nav {
+    color: white;
+    font-weight: bold;
+    padding: 15px;
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
